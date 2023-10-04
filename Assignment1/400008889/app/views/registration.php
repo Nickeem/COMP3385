c1f7b6166b3721ca75c5b2be36594abfcc15e919
Nickeem <vacation1478@gmail.com>
2023-10-03 21:25:46 -0400

    Added pico css

--- Assignment1/400008889/app/views/registration.php
+++ Assignment1/400008889/app/views/registration.php
@@ -19,35 +19,50 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/pico.min.css">
     <title>Registration</title>
 </head>
 <body>
+    <main class="container">
     <form action="index.php" method="post">
-        <div>
-            <label for="username">Enter Username: </label>
-            <input type="text" id="username" name="username">
-            <p class="error username <?php if ($username_error != '') {echo 'show';}?>"> <?php echo $username_error ?> </p>
-        </div> 
         
-        <div>
-            <label for="email">Enter Email: </label>
-            <input type="email" id="email" name="email" required>
-            <p class="error email <?php if ($email_error != '') {echo 'show';}?>"> <?php echo $email_error ?></p>
-        </div>
-        <div>
-            <label for="password">Enter password: </label>
-            <input type="password" id="password" name="password">
-            <p class="error password <?php if ($email_error != '') {echo 'show';}?>"> <?php echo $password_error ?></p>
-        </div>
-         
-         
-         <!--<label for="confirm_password"><input type="text" id="confirm_password"></label> -->
-         <input type="submit"  value="Submit">
-    </form>
+
+        <fieldset>
+            <label for="username">
+                Enter Username: 
+                <input type="text" id="username" name="username" placeholder="Username" required  <?php if ($username_error != '') {echo "aria-invalid=\"true\"";} ?>>
+                <small class="error username <?php if ($username_error != '') {echo 'show';}?>"> <?php echo $username_error ?> </small>
+            </label>
+        </fieldset>
+            
+            <label for="email">
+                Enter Email: 
+                <input type="email" id="email" name="email" placeholder="Email" required <?php if ($email_error != '') {echo "aria-invalid=\"true\"";}?>  >
+                <small class="error email <?php if ($email_error != '') {echo 'show';}?>"> <?php echo $email_error ?></small>
+            </label>
+
+        
+    
+        <label for="password">
+            Enter password:
+            <input type="password" id="password" name="password" placeholder="Password" required <?php if ($email_error != '') {echo "aria-invalid=\"true\"";}?>>
+            <small class="error password <?php if ($email_error != '') {echo 'show';}?>"> <?php echo $password_error ?></small>
+        </label>
+       
+     
+     
+        <!--<label for="confirm_password"><input type="text" id="confirm_password"></label> -->
+        <input type="submit"  value="Register">
+   
+</form>
+    </main>
+    
+
+    
 </body>
 </html>
 
