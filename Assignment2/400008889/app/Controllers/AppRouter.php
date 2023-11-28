ba5b678a349017631b3459c3a4b16523fc7944d2
Nickeem <vacation1478@gmail.com>
2023-11-27 21:43:07 -0400

    Added route callbacks

--- Assignment2/400008889/app/Controllers/AppRouter.php
+++ Assignment2/400008889/app/Controllers/AppRouter.php
@@ -1,14 +1,15 @@
 <?php
+namespace App\Controllers;
 include 'C:\xampp\htdocs\COMP3385\COMP3385\Assignment2\400008889\framework/autoload.php';
 
-class AppRouter extends Abstracts\Router 
+class AppRouter extends \Abstracts\Router 
 {
-    public function __construct()
+    public function __construct($root_uri='')
     {
-        parent::__construct();
-        $this->addRoute('/', []);
-        $this->addRoute('/Login/', []);
-        $this->addRoute('/Dashboard/', []);
-        $this->addRoute('/Logout/', []);
+        parent::__construct($root_uri);
+        // $this->addRoute('/', []);
+        $this->addRoute('/Login', ['\App\Controllers\Login', 'executeHelper']);
+        $this->addRoute('/Dashboard', ['\App\Controllers\Dashboard', 'execute']);
+        $this->addRoute('/Logout', ['\App\Controllers\Logout', 'execute']);
     }
 }
