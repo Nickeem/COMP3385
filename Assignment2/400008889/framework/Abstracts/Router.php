e4dbbd35bd6efb9f3cb986c40f62855f81689aba
Nickeem <vacation1478@gmail.com>
2023-11-27 21:28:31 -0400

    root uri added as property

--- Assignment2/400008889/framework/Abstracts/Router.php
+++ Assignment2/400008889/framework/Abstracts/Router.php
@@ -9,22 +9,48 @@ abstract class Router implements \Interfaces\RouterInterface
 {
     // attributes
     protected $routes;
+    protected $root_uri;
 
-    public function __construct()
+    public function __construct($root_uri='')
     {
         $routes = array();
+        $this->root_uri = $root_uri;
     }
 
-    public function addRoute($path, $handler, $method='get') 
+    public function setRootURI($uri)
+    {
+        $this->root_uri = $uri;
+    }
+
+    public function addRoute($path, $handler) 
     {
         if(isset($this->routes[$path])) 
         {
             throw new \Exceptions\RouteException("Route '{$path}' already exists");
         }
 
+        $isCallable = true;
+
         // handler should either be a function or array containing class and method. In any case it should be callable
-        if (!is_callable($handler))
+        if (is_array($handler))
         {
+            if (!is_callable([new $handler[0], $handler[1]]))
+            {
+                $isCallable = false;
+            }    
+        } 
+        else 
+        {
+            if (!is_callable($handler))
+            {
+                $isCallable = false;
+            }
+        }
+        
+        
+        if (!$isCallable)
+        {
+            var_dump($handler);
             throw new \Exceptions\RouteException("handler passed to 'addRoute' is invalid, handler must be callable"); 
         }
         
@@ -81,7 +107,7 @@ abstract class Router implements \Interfaces\RouterInterface
         // $path = $_REQUEST['path'];
         // $this->route($path);
 
-        $raw_path = $_SERVER['REQUEST_URI']; // doing it this way lets the function not be rule dependent - my own assumption.
+        $raw_path = $_SERVER['REQUEST_URI'] ?? '/'; // doing it this way lets the function not be rule dependent - my own assumption.
 
         // clean raw_path
         // Remove trailing slashes
@@ -94,7 +120,7 @@ abstract class Router implements \Interfaces\RouterInterface
         // Remove redundant slashes
         $path = preg_replace('#/{2,}#', '/', $path);
 
-        $search = '/'.preg_quote(APP_URI, '/').'/';
+        $search = '/^'.preg_quote($this->root_uri, '/').'/';
         $path = preg_replace($search, '', $path, 1); 
         $this->route($path);
     }
