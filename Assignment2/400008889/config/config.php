cb68a5df76ce360ea8d8e8067565ccd08a527bf6
Nickeem <vacation1478@gmail.com>
2023-11-27 21:43:54 -0400

    added more utility config variables

--- Assignment2/400008889/config/config.php
+++ Assignment2/400008889/config/config.php
@@ -1,9 +1,13 @@
 <?php
-define('APP_DIR', 'C:\xampp\htdocs\COMP3385\COMP3385\Assignment2\400008889\app');
-define('FRAMEWORK_DIR', 'C:\xampp\htdocs\COMP3385\COMP3385\Assignment2\400008889\framework');
 define('ROOT_DIR', 'C:\xampp\htdocs\COMP3385\COMP3385\Assignment2\400008889');
-define('HOST', 'localhost');
+define('ROOT_URI', '\COMP3385\COMP3385\Assignment2\400008889');
+define('FRAMEWORK_DIR', ROOT_DIR.'\framework');
 define('APP_URI', '/COMP3385/COMP3385/Assignment2/400008889/app');
+define('APP_DIR', ROOT_DIR.'\app');
+define('TPL_DIR', ROOT_DIR.'\tpl');
+define('CSS_DIR', ROOT_URI.'\css');
+define('JS_DIR', ROOT_URI.'\js');
+define('HOST', 'localhost');
 define('DATABASE', 'user_management_system');
 define('USERNAME', 'root');
 define('PASSWORD', '');
